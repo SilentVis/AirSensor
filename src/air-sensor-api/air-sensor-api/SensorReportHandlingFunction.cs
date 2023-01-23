759cb1491b371ff028b221e914f5e6066ff49695
Serhii Tereshchenko <steres@softserveinc.com>
2023-01-23 14:06:01 +0200

    feat: added func app

---
+++ src/air-sensor-api/air-sensor-api/SensorReportHandlingFunction.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AirSensor.FunctionApp
+{
+    public class SensorReportHandlingFunction
+    {
+        [FunctionName("Sensor readings endpoint")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest request, ILogger log)
+        {
+            return new OkObjectResult("Success message");
+        }
+    }
+}
